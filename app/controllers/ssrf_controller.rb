9a3229a945b55910d6a8a1c690b09e73bcd7ac4b
Cohen Carlisle <cohencarlisle+github@pm.me>
2023-10-24 16:44:56 -0400

    but what if it matches anything?

--- app/controllers/ssrf_controller.rb
+++ app/controllers/ssrf_controller.rb
@@ -1,7 +1,7 @@
 class SsrfController < ApplicationController
  def index
    conn = Faraday.new("https://api.github.com/users/")
-   username = params[:username].to_s
+   username = params[:username].to_s.match(/.*/)[0]
    resp = conn.get(username, {}, fake_authorization: "Bearer my_secret_token") 
    render plain: "GET #{resp.env.url} with headers #{resp.env.request_headers} returned:\n\n#{resp.body}"
  end
