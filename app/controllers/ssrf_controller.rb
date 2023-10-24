b1c7574615ae8f95a682d78d6040f4a77d35656f
Cohen Carlisle <cohencarlisle+github@pm.me>
2023-10-24 16:04:28 -0400

    a working github api, woo!

---
+++ app/controllers/ssrf_controller.rb
@@ -0,0 +1,7 @@
+class SsrfController < ApplicationController
+ def index
+   conn = Faraday.new("https://api.github.com/users/")
+   resp = conn.get(params[:username], {}, fake_authorization: "Bearer my_secret_token") 
+   render plain: "GET #{resp.env.url} with headers #{resp.env.request_headers} returned:\n\n#{resp.body}"
+ end
+end
