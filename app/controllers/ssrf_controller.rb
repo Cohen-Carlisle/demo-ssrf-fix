6b286f0e16dedc5befd14ae346d276975afaaf46
Cohen Carlisle <cohencarlisle+github@pm.me>
2023-10-25 17:23:44 -0400

    last desperate attempt to evade the codeql gods

--- app/controllers/ssrf_controller.rb
+++ app/controllers/ssrf_controller.rb
@@ -1,7 +1,7 @@
 class SsrfController < ApplicationController
  def index
    conn = Faraday.new("https://api.github.com/users/")
-   username = params[:username].to_s.scan(/.+/).join
+   username = params[:username].to_s.scan(/.+/).join("")
    resp = conn.get(username, {}, fake_authorization: "Bearer my_secret_token") 
    render plain: "GET #{resp.env.url} with headers #{resp.env.request_headers} returned:\n\n#{resp.body}"
  end
