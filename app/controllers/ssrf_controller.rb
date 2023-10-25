1d2f7776509e43e2e322dc32aa7825edd2e4a6fd
Cohen Carlisle <cohencarlisle+github@pm.me>
2023-10-25 17:09:39 -0400

    sanitize username in path

--- app/controllers/ssrf_controller.rb
+++ app/controllers/ssrf_controller.rb
@@ -1,7 +1,8 @@
 class SsrfController < ApplicationController
  def index
    conn = Faraday.new("https://api.github.com")
-   path = "/users/" + params[:username]
+   username = params[:username].gsub(/[^A-Za-z0-9-]+/, "")
+   path = "/users/" + username
    resp = conn.get(path, {}, fake_authorization: "Bearer my_secret_token") 
    render plain: "GET #{resp.env.url} with headers #{resp.env.request_headers} returned:\n\n#{resp.body}"
  end
