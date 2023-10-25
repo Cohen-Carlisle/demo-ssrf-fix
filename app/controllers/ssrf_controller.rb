35ac51774cffd1ad69f18c2ca1863347ef749630
Cohen Carlisle <cohencarlisle+github@pm.me>
2023-10-25 17:02:41 -0400

    let's try a nice safe id instead

--- app/controllers/ssrf_controller.rb
+++ app/controllers/ssrf_controller.rb
@@ -1,8 +1,8 @@
 class SsrfController < ApplicationController
  def index
-   conn = Faraday.new("https://api.github.com/users/")
-   username = params[:username]
-   resp = conn.get(username, {}, fake_authorization: "Bearer my_secret_token") 
+   conn = Faraday.new("https://api.github.com/user/")
+   user_id= params[:user_id]
+   resp = conn.get(user_id, {}, fake_authorization: "Bearer my_secret_token") 
    render plain: "GET #{resp.env.url} with headers #{resp.env.request_headers} returned:\n\n#{resp.body}"
  end
 end
