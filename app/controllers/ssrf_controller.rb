aad1c08a59d67e842815d3e13f1095998c5e8d8a
Cohen Carlisle <cohencarlisle+github@pm.me>
2023-10-25 17:19:13 -0400

    try raising on unexpected format

--- app/controllers/ssrf_controller.rb
+++ app/controllers/ssrf_controller.rb
@@ -1,7 +1,8 @@
 class SsrfController < ApplicationController
  def index
    conn = Faraday.new("https://api.github.com/users/")
-   username = params[:username]
+   username = params[:username].to_s
+   raise "invalid username" unless username.match?(/\A[A-Za-z0-9-]+\z/)
    resp = conn.get(username, {}, fake_authorization: "Bearer my_secret_token") 
    render plain: "GET #{resp.env.url} with headers #{resp.env.request_headers} returned:\n\n#{resp.body}"
  end
